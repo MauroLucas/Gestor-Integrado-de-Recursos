fd844f4dc323840e654aa5327d5950f869066405
giannativo <giannativo@yahoo.com.ar>
2018-11-04 16:53:44 -0300

    agrego mensaje de error

--- Project/Server/Server/flaskr/auth.py
+++ Project/Server/Server/flaskr/auth.py
@@ -35,6 +35,32 @@ def register():
     return render_template('register.html')
 
 
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        if not db.session.query(User.query.filter(User.username == username).exists()).scalar():
+            error = 'User or password is not valid.'
+        else:
+            user = db.session.query(User).filter(User.username == username).one()
+            if user.password != request.form['password']:
+                error = 'User or password is not valid.'
+
+        if error is None:
+            return redirect(url_for('auth.register_succesful'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+
 @bp.route('/register_succesful')
 def register_succesful():
     return render_template('register_success.html')
