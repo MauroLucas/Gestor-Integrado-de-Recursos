d26044fadfdebce07240764819d378ae5e9c9561
giannativo <giannativo@yahoo.com.ar>
2018-11-16 23:31:21 -0300

    agregando query para traer

--- Project/Server/Server/flaskr/ControladorAgregarRecurso.py
+++ Project/Server/Server/flaskr/ControladorAgregarRecurso.py
@@ -22,11 +22,16 @@ def enter_resource():
         if not category:
             error = 'The resource needs category'
         if error is None:
-            db.session.add(Recurso(recurso=resource, descripcion=description, fecha=time.clock()))
+            user = db.session.query(Usuario).filter(Usuario.username == session['user']).one()
+            recurso = Recurso(recurso=resource, descripcion=description, fecha=time.clock())
+            db.session.add(recurso)
+            db.session.flush()
+            if db.session.query(Categoria.query.filter(Categoria.nombre == category).exists()).scalar():
+                categoria = db.session.query(Categoria).filter(Categoria.nombre == category).one()
+                db.session.add(CategoriaXRecurso(db.session))
+            else:
+                db.session.add(Categoria(nombre=category, id_usuario=user.id_usuario))
             db.session.commit()
-            ''''if db.session.query(Categoria.query.filter(Categoria.nombre == category).exists()).scalar():
-                db.session.add(CategoriaXRecurso(db.session)
-            else db.session.add(Categoria(nombre=category,id_usuario=db.session.fi))'''''
             return redirect(url_for('auth.login_succesful'))
         flash(error)
     return render_template('agregarRecurso.html')
