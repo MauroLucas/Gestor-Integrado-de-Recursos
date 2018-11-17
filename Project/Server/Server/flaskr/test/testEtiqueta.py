5cdd8fc470a3d5cab263d917f8a6a2d816b96b02
MauroLucas <m_l_pereyra@hotmail.com>
2018-11-17 12:11:34 -0300

    Modificaciones en Clase test

--- Project/Server/Server/flaskr/test/testEtiqueta.py
+++ Project/Server/Server/flaskr/test/testEtiqueta.py
@@ -3,7 +3,13 @@ from flaskr.base import Etiqueta
 
 
 #Agregar
-etiqueta = Etiqueta(nombre='python2')
-db.session.add(etiqueta)
-db.session.commit()
+#etiqueta = Etiqueta(nombre='python2')
+#db.session.add(etiqueta)
+#db.session.commit()
+etiqueta1 = db.session.query(Etiqueta).filter(Etiqueta.id_etiqueta == 1).one()
+
+#recuros de esa etiqueta
+
+for recurso in etiqueta1.recursos:
+    print recurso.recurso.recurso
 
--- Project/Server/Server/flaskr/test/testRecurso.py
+++ Project/Server/Server/flaskr/test/testRecurso.py
@@ -2,7 +2,15 @@ from flaskr.base import db
 from flaskr.base import Categoria,Recurso
 import datetime
 
-recurso = Recurso(recurso='http://',descripcion='Paginaredsocial',fecha=datetime.datetime.now())
+#recurso = Recurso(recurso='http://',descripcion='Paginaredsocial',fecha=datetime.datetime.now())
 
-db.session.add(recurso)
-db.session.commit()
+#db.session.add(recurso)
+#db.session.commit()
+recurso1 = db.session.query(Recurso).filter(Recurso.id_recurso == 2).one()
+#Traer todas las categorias de ese recurso
+print'categorias del recurso'
+for cate in recurso1.categorias:
+    print cate.categoria.nombre
+print 'etiquetas del recurso'
+for etiqueta in recurso1.etiquetas:
+    print etiqueta.etiqueta.nombre
