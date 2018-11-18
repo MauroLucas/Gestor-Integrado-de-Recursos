7fffdacdafa19e1e36b2fe7dda03d948eb40d903
MauroLucas <m_l_pereyra@hotmail.com>
2018-11-18 17:05:50 -0300

    Modifico controlador para agregar admin a la lista de participantes

--- Project/Server/Server/flaskr/ControladorCrearGrupo.py
+++ Project/Server/Server/flaskr/ControladorCrearGrupo.py
@@ -2,7 +2,7 @@ from sqlalchemy.sql.expression import func
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
-from base import db, Usuario
+from base import db, Usuario,UsuarioXGrupo
 from flask import session
 from base import *
 
@@ -22,7 +22,12 @@ def crear_grupo():
         if error is None:
             user = db.session.query(Usuario).filter(Usuario.username == session['user']).one()
             grupo1 = Grupo(nombre=grupo, id_admin=user.id_usuario)
+            #agregar admin a la lista de usuarios
+
+
             db.session.add(grupo1)
+            db.session.flush()
+            db.session.add(UsuarioXGrupo(id_grupo=grupo1.id_grupo,id_usuario=user.id_usuario))
             #db.session.flush()
             #if db.session.query(Categoria.query.filter(Categoria.nombre == category).exists()).scalar():
                 #categoria = db.session.query(Categoria).filter(Categoria.nombre == category).one()
