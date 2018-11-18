d73ff7294170260e8e977d91dcd5e97d799b3133
germoreno36 <ger.moreno36@gmail.com>
2018-11-18 17:31:16 -0300

    Buscar por etiqueta

--- Project/Server/Server/flaskr/test/testRecursosPorEtiqueta.py
+++ Project/Server/Server/flaskr/test/testRecursosPorEtiqueta.py
@@ -2,23 +2,17 @@ from flaskr.base import db
 from flaskr.base import Categoria,Usuario,Etiqueta
 
 
-usuario = db.session.query(Usuario).filter(Usuario.username == 'Mauro').one()
-etiqueta1 = db.session.query(Etiqueta).filter(Etiqueta.nombre == 'cumbia').one()
-
-print etiqueta1.id_etiqueta
+usuario = db.session.query(Usuario).filter(Usuario.username == 'qwerty').one()
+etiqueta1 = db.session.query(Etiqueta).filter(Etiqueta.nombre == 'python').one()
 
 recursosEnEtiqueta = []
-
 for categoria in usuario.categorias:
-    print 'Categoria ' + str(categoria.nombre)
     for recurso in categoria.recursos:
-        print ' Recurso ' + str(recurso.recurso.recurso)
         for etiqueta in recurso.recurso.etiquetas:
-            print '  Etiqueta ' + str(etiqueta.etiqueta.nombre)
             if etiqueta.etiqueta.id_etiqueta==etiqueta1.id_etiqueta:
                 recursosEnEtiqueta.append(recurso.recurso)
 
-print 'Mis re cursos de la etiqeuta ' + str(etiqueta1.nombre)
+print 'Rcursos de la etiqueta ' + str(etiqueta1.nombre)
 for recu in recursosEnEtiqueta:
     print recu.recurso
 
